06df4ce08f9cc5a168e8e4918b31941d87985987
Anurag <anurag.icfai2009@gmail.com>
2022-07-28 10:27:24 +0530

    This is my second  code push in

--- mongodbtest.py
+++ mongodbtest.py
@@ -8,6 +8,24 @@ d= {
 "email1":"anurag1.icfai2009@gmail.com",
 "surname1":"Gupta1"
 }
+
+d= {
+"name1":"anurag1",
+"email1":"anurag1.icfai2009@gmail.com",
+"surname1":"Gupta1"
+}
+
+d= {
+"name1":"anurag1",
+"email1":"anurag1.icfai2009@gmail.com",
+"surname1":"Gupta1"
+}
+
+d= {
+"name1":"anurag1",
+"email1":"anurag1.icfai2009@gmail.com",
+"surname1":"Gupta1"
+}
 db1=client['mongotest']
 coll=db1['test']
 coll.insert_one(d)
